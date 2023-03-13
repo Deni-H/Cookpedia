218319f3da43e4294a0a96ebf8d7ef8a83954059
Deni Hilhamsyah <denyhilhamsyh@gmail.com>
2023-03-13 16:17:54 +0700

    Adding Onboarding.kt screen

--- app/src/main/java/com/denihilhamsyah/cookpedia/MainActivity.kt
+++ app/src/main/java/com/denihilhamsyah/cookpedia/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.denihilhamsyah.cookpedia.ui.screen.Onboarding
 import com.denihilhamsyah.cookpedia.ui.theme.CookpediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Onboarding()
                 }
             }
         }
---
+++ app/src/main/java/com/denihilhamsyah/cookpedia/ui/screen/Onboarding.kt
@@ -0,0 +1,67 @@
+package com.denihilhamsyah.cookpedia.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.denihilhamsyah.cookpedia.R
+
+@Composable
+fun Onboarding() {
+    Box {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.bg_onboarding),
+            contentDescription = "bg_onboarding",
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = stringResource(R.string.premium_recipes)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.Transparent,
+                                Color.Black
+                            )
+                        )
+                    )
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(R.string.lets_cooking))
+                Text(text = stringResource(R.string.best_recipes))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = stringResource(R.string.start_cooking))
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_right),
+                        contentDescription = "arrow_right"
+                    )
+                }
+            }
+        }
+    }
+}
